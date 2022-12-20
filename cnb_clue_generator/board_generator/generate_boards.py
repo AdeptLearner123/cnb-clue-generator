a6ed89325e8ad7273e035eb6f52aef8e49211638
Nalu Zou <naluzou@Nalus-MacBook-Pro.local>
2022-12-19 22:31:11 -0800

    Add word pairs evaluation set

--- cnb_clue_generator/board_generator/generate_boards.py
+++ cnb_clue_generator/board_generator/generate_boards.py
@@ -14,8 +14,12 @@ def parse_args():
     parser = ArgumentParser()
     parser.add_argument("-n", "--number", type=int, required=True)
     parser.add_argument("-f", "--file-name", type=str, required=True)
+    parser.add_argument("--min-pos", type=int, required=False, default=1)
+    parser.add_argument("--max-pos", type=int, required=False, default=POSITIVE_WORDS)
+    parser.add_argument("--min-neg", type=int, required=False, default=BLANK_WORDS)
+    parser.add_argument("--max-neg", type=int, required=False, default=BLANK_WORDS + ENEMY_WORDS)
     args = parser.parse_args()
-    return args.number, args.file_name
+    return args.number, args.file_name, args.min_pos, args.max_pos, args.min_neg, args.max_neg
 
 
 def get_boards(file_path):
@@ -30,7 +34,7 @@ def get_boards(file_path):
 
 
 def main():
-    number, file_name = parse_args()
+    number, file_name, min_pos, max_pos, min_neg, max_neg = parse_args()
 
     with open(CARDWORDS, "r") as file:
         cardwords = file.read().splitlines()
@@ -40,15 +44,15 @@ def main():
 
     for _ in range(number):
         id = str(uuid.uuid4())
-        boards[id] = create_board(cardwords)
+        boards[id] = create_board(cardwords, min_pos, max_pos, min_neg, max_neg)
 
     with open(file_path, "w+") as file:
         file.write(json.dumps(boards, indent=4))
 
 
-def create_board(cardwords):
-    num_pos = random.randint(1, POSITIVE_WORDS)
-    num_neg = random.randint(BLANK_WORDS, BLANK_WORDS + ENEMY_WORDS)
+def create_board(cardwords, min_pos, max_pos, min_neg, max_neg):
+    num_pos = random.randint(min_pos, max_pos)
+    num_neg = random.randint(min_neg, max_neg)
     words = random.sample(cardwords, num_pos + num_neg)
     pos_words = words[:num_pos]
     neg_words = words[num_pos:]
