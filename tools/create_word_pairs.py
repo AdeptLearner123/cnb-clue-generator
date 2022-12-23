d413b6acebf0c657a6c6fc90bbbd63d54494ba66
Nalu Zou <naluzou@Nalus-MacBook-Pro.local>
2022-12-22 18:24:03 -0800

    More word pairs

--- tools/create_word_pairs.py
+++ tools/create_word_pairs.py
@@ -1,11 +1,39 @@
 from config import WORD_PAIRS, CARDWORDS
 
+import json
+import random
+
 def main():
     with open(CARDWORDS, "r") as file:
         words = file.read().splitlines()
     
-    word_pairs
+    with open(WORD_PAIRS, "r") as file:
+        word_pairs = json.loads(file.read())
 
+    while True:
+        sample = random.sample(words, 4)
+        print("Words:", sample)
+        word1 = input("word 1:")
+        word2 = input("word 2:")
+
+        if word1 not in sample or word2 not in sample:
+            print("Invalid words")
+            continue
+        
+        clue = input("clue:")
+        explanation_1 = input("explanation 1:")
+        explanation_2 = input("explanation 2:")
+
+        word_pairs.append({
+            "word1": word1,
+            "word2": word2,
+            "clue": clue,
+            "explanation1": explanation_1,
+            "explanation2": explanation_2
+        })
+
+        with open(WORD_PAIRS, "w+") as file:
+            file.write(json.dumps(word_pairs, indent=4))
 
 
 if __name__ == "__main__":
